9856c860e88e7bbd5f04858f9fa3d52714e8a60f
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-02-05 10:28:45 +0900

    Update screentimeAppLimit-Bridging-Header.h

--- screentime/screentimeAppLimit-Bridging-Header.h
+++ screentime/screentimeAppLimit-Bridging-Header.h
@@ -2,4 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "grant_full_disk_access.h"
+#import "Exploit/fun/helpers.h"
+#import "Exploit/fun/fun.h"
+#import "Exploit/fun/grant_full_disk_access.h"
+#import "Exploit/fun/krw.h"
+#import "Exploit/fun/utils.h"
+#import "Exploit/fun/offsets.h"
+#import "Exploit/fun/thanks_opa334dev_htrowii.h"
+#import "StatusManager.h"
